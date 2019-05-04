b4374c4dea28f3c569e2a69965fa85646e3c012c
Ujjwol Kayastha <uzol123@gmail.com>
2019-05-04 22:31:25 +0545

    UI designs and validations

--- app/src/main/java/com/example/assignment3onlineclothshop/ui/main/ShowItemsActivity.java
+++ app/src/main/java/com/example/assignment3onlineclothshop/ui/main/ShowItemsActivity.java
@@ -1,9 +1,14 @@
 package com.example.assignment3onlineclothshop.ui.main;
 
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.View;
 import android.widget.AbsListView;
 
 import com.example.assignment3onlineclothshop.Adapter.ItemAdapter;
@@ -29,11 +34,13 @@ public class ShowItemsActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerview);
 
-        readItems();
-
-        recyclerView.setAdapter(new ItemAdapter(getApplicationContext(),itemsList));
         recyclerView.setLayoutManager(new GridLayoutManager(this,2));
-    }
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(3), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(new ItemAdapter(getApplicationContext(),itemsList));
+
+        readItems();
+       }
 
     private void readItems() {
         try {
@@ -43,7 +50,7 @@ public class ShowItemsActivity extends AppCompatActivity {
             BufferedReader br=new BufferedReader(isr);
             String line="";
 
-            while ((line=br.readLine()) != null){
+            while ((line = br.readLine()) != null){
                 String[] parts = line.split("->");
                 String imgDrawagble =parts[2];
                 int id=getResources().getIdentifier(imgDrawagble,"drawable",getPackageName());
@@ -52,5 +59,47 @@ public class ShowItemsActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 }
